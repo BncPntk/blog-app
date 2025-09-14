4d78ffb5008dcc5a616eb0df6244c296fadb6186
BncPntk <pntkbence@gmail.com>
2025-09-15 01:14:53 +0200

    feat(seeder): add Post and Comment factories with sample data

---
+++ database/seeders/SampleDataSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class SampleDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::factory()->count(3)->create();
+
+        $users->each(function (User $u) {
+            Post::factory()->count(3)->for($u)->create()->each(function (Post $post) use ($u) {
+                Comment::factory()->count(2)->for($post)->for($u)->create();
+                Comment::factory()->count(2)->for($post)->create(['user_id' => null]);
+            });
+        });
+    }
+}
